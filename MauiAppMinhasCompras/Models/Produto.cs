e1ba538fb23aced5f264eb87d2c8856d24e58c2b
juliosntts <julio0123123123@gmail.com>
2025-03-28 12:07:57 -0300

    5.0

--- MauiAppMinhasCompras/Models/Produto.cs
+++ MauiAppMinhasCompras/Models/Produto.cs
@@ -9,11 +9,46 @@ namespace MauiAppMinhasCompras.Models
 {
     public class Produto
     {
+        private string _descricao;
+        private double _quantidade;
+        private double _preco;
+
         [PrimaryKey, AutoIncrement]
         public int Id { get; set; }
-        public string Descricao { get; set; }
-        public double Quantidade { get; set; }
-        public double Preco { get; set; }
+        public string Descricao
+        {
+            get => _descricao;
+            set
+            {
+                if (value == null)
+                {
+                    throw new Exception("Por favor, preencha a descrição");
+                }
+                _descricao = value;
+            }
+        }
+        public double Quantidade { 
+            get => _quantidade;
+            set 
+            {
+                if (value <= 0)
+                {
+                    throw new Exception("Por favor, preencha uma quantidade valida");
+                }
+                _quantidade = value;
+            }
+        }
+        public double Preco { 
+            get => _preco;
+            set 
+            {
+                if (value <= 0)
+                {
+                    throw new Exception("Por favor, preencha um preço valido");
+                }
+                _preco = value;
+            }
+        }
         public double Total { get => Quantidade * Preco; }
     }
 }
